b8f47ab11eb3d4b6f57c51ca5e35c4461b35b7d2
ytc338 <yf87008@gmail.com>
2026-01-09 01:08:42 -0800

    Remove debug logs from DBPrefsWindowController methods to clean up output

--- Welly/DBPrefsWindowController.m
+++ Welly/DBPrefsWindowController.m
@@ -97,10 +97,8 @@ static DBPrefsWindowController *_sharedPrefsWindowController = nil;
 }
 
 - (void)awakeFromNib {
-    NSLog(@"[DEBUG] DBPrefsWindowController: awakeFromNib - Start");
     [self setupMenuOfURLScheme:@"telnet" forPopUpButton:_telnetPopUpButton];
     [self setupMenuOfURLScheme:@"ssh" forPopUpButton:_sshPopUpButton];
-    NSLog(@"[DEBUG] DBPrefsWindowController: awakeFromNib - End");
 }
 
 - (instancetype)initWithWindow:(NSWindow *)window
@@ -139,7 +137,6 @@ static DBPrefsWindowController *_sharedPrefsWindowController = nil;
                                                        defer:YES];
     [window setCollectionBehavior:NSWindowCollectionBehaviorFullScreenAuxiliary | NSWindowCollectionBehaviorCanJoinAllSpaces];
     self.window = window;
-    NSLog(@"[DEBUG] DBPrefsWindowController: windowDidLoad (Window Created)");
     contentSubview = [[NSView alloc] initWithFrame:self.window.contentView.frame];
     contentSubview.autoresizingMask = (NSViewMinYMargin | NSViewWidthSizable);
     [self.window.contentView addSubview:contentSubview];
@@ -298,7 +295,6 @@ static DBPrefsWindowController *_sharedPrefsWindowController = nil;
 
 - (IBAction)showWindow:(id)sender 
 {
-    NSLog(@"[DEBUG] DBPrefsWindowController: showWindow Called");
     // This forces the resources in the nib to load.
     (void)self.window;
     
@@ -328,7 +324,6 @@ static DBPrefsWindowController *_sharedPrefsWindowController = nil;
     [self.window center];
     
     [super showWindow:sender];
-    NSLog(@"[DEBUG] DBPrefsWindowController: showWindow Finished");
 }
 
 
